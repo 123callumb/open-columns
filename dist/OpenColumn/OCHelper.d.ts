4fc211c11f6562ef098720d4124ead978ae54c27
123callumb <123callumb@gmail.com>
2022-08-18 21:10:10 +0100

    lol

--- dist/OpenColumn/OCHelper.d.ts
+++
@@ -1,4 +0,0 @@
-import { OCDataHeaderOptions } from "./OCTypes";
-export default class OCHelper {
-    static CreateHeaders<T>(headerOptions: string[] | OCDataHeaderOptions<T>[]): OCDataHeaderOptions<T>[];
-}
